8853970615806a7fe1ff4c01f30e2299747fa942
Jatin Mittal <112621022+Jat21in@users.noreply.github.com>
2025-06-06 14:49:52 +0530

    Update Summary.jsx

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import './summary.css';
 
@@ -5,6 +6,10 @@ function Summary() {
   const location = useLocation();
   const navigate = useNavigate();
   const data = location.state;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   if (!data) {
     return <div>No data submitted. <button onClick={() => navigate('/')}>Go Back</button></div>;
